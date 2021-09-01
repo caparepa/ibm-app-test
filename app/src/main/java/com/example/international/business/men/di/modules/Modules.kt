e80c9fb771a46192b07eae7f21350d42783aa528
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-01 19:39:57 -0400

    Control commit

--- app/src/main/java/com/example/international/business/men/di/modules/Modules.kt
+++ app/src/main/java/com/example/international/business/men/di/modules/Modules.kt
@@ -1,13 +1,11 @@
 package com.example.international.business.men.di.modules
 
-import android.content.Context
 import com.example.international.business.men.data.InternationalBusinessMenDatabase
 import com.example.international.business.men.data.db.dao.ProductDao
 import com.example.international.business.men.network.api.ApiClient
 import com.example.international.business.men.network.interceptor.ConnectivityInterceptor
 import com.example.international.business.men.network.interceptor.ConnectivityInterceptorImpl
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val networkModule = module {
@@ -16,31 +14,36 @@ val networkModule = module {
 }
 
 val repositoryModule = module {
-
+    //TODO: YOUR CODE HERE
 }
 
 val localModule = module {
-
+    //TODO: YOUR CODE HERE
 }
 
 val dataModule = module {
-
+    //TODO: YOUR CODE HERE
 }
 
 val databaseModule = module {
-    single { InternationalBusinessMenDatabase.invoke(androidContext()) }
+    /*single { InternationalBusinessMenDatabase.invoke(androidContext()) }
 
     fun provideProductDao(database: InternationalBusinessMenDatabase): ProductDao {
         return database.getProductDao()
     }
 
-    single { provideProductDao(get()) }
+    single { provideProductDao(get()) }*/
+
+    //TODO: YOUR CODE HERE
+    //NOTE: since the data source return random values with each request,
+    //there's no point on persisting in the database
+
 }
 
 val viewModelModule = module {
-
+    //TODO: YOUR CODE HERE
 }
 
 val utilsModule = module {
-
+    //TODO: YOUR CODE HERE
 }
