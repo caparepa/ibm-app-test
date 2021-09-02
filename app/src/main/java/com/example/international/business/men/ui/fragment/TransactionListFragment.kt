bfd8de903e7951b4948cc8f87f34ef5d5d02c26b
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-02 14:08:38 -0400

    control commit

--- app/src/main/java/com/example/international/business/men/ui/fragment/TransactionListFragment.kt
+++ app/src/main/java/com/example/international/business/men/ui/fragment/TransactionListFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.international.business.men.data.model.TransactionItem
 import com.example.international.business.men.databinding.FragmentTransactionListBinding
+import com.example.international.business.men.ui.adapter.base.DynamicAdapter
+import com.example.international.business.men.ui.adapter.base.ItemModel
+import com.example.international.business.men.ui.adapter.item.model.TransactionItemModel
+import com.example.international.business.men.ui.adapter.type.factory.TransactionItemTypeFactoryImpl
 import com.example.international.business.men.ui.viewmodel.ProductTransactionViewModel
 import com.example.international.business.men.utils.toastLong
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -51,6 +56,45 @@ class TransactionListFragment : Fragment(), KoinComponent {
 
     private fun loadTransactionList(sku: String) {
 
+    }
+
+    private fun observeViewModel() = productTransactionViewModel.run {
+
+    }
+
+    private fun setUpTransactionListAdapter(list: List<TransactionItem>) {
+
+    }
+
+    private fun getTransactionListAdapter(list: List<TransactionItem>): DynamicAdapter {
+        return DynamicAdapter(
+            typeFactory = TransactionItemTypeFactoryImpl(),
+            items = getListForAdapter(list.toMutableList()),
+            onClick = onItemClick
+        )
+    }
+
+    private fun getListForAdapter(list: MutableList<TransactionItem>?): List<ItemModel> {
+        var result = mutableListOf<ItemModel>()
+        list?.forEach {
+            result.add(TransactionItemModel(it))
+        }
+        return result
+    }
+
+    private var onItemClick: (ItemModel, String) -> Unit = { item, action ->
+        val transaction : TransactionItemModel = item as TransactionItemModel
+        val currency = item.model.currency
+        val amountOriginal = item.model.amount
+        val amountEuro = item.model.amount
+        when (action) {
+            "no_action" -> {
+                requireActivity().toastLong("Amount - $currency $amountOriginal / EUR $amountEuro")
+            }
+            else -> {
+                requireActivity().toastLong("NO ACTION")
+            }
+        }
     }
 }
 
