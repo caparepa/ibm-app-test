863770f878ee900d9b60fabdcdbe835ba9e730a1
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-02 11:22:05 -0400

    holder (wip)

--- app/src/main/java/com/example/international/business/men/ui/adapter/holder/TransactionItemViewHolder.kt
+++ app/src/main/java/com/example/international/business/men/ui/adapter/holder/TransactionItemViewHolder.kt
@@ -1,9 +1,13 @@
 package com.example.international.business.men.ui.adapter.holder
 
 import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.international.business.men.R
 import com.example.international.business.men.ui.adapter.base.DynamicAdapterViewHolder
 import com.example.international.business.men.ui.adapter.base.ItemModel
 import com.example.international.business.men.ui.adapter.item.model.TransactionItemModel
+import com.example.international.business.men.utils.setOneOffClickListener
 
 class TransactionItemViewHolder(private val view: View) :
     DynamicAdapterViewHolder<TransactionItemModel>(view) {
@@ -11,7 +15,10 @@ class TransactionItemViewHolder(private val view: View) :
     lateinit var item: TransactionItemModel
     lateinit var onClick: (item: TransactionItemModel, action: String) -> Unit
 
-
+    private val itemLayout = itemView.findViewById<ConstraintLayout>(R.id.clItemLayout)
+    private val skuValue = itemView.findViewById<TextView>(R.id.tvTxSkuValue)
+    private val amountOriginal = itemView.findViewById<TextView>(R.id.tvAmountOriginal)
+    private val amountEuro = itemView.findViewById<TextView>(R.id.tvAmountEuro)
 
     override fun bind(
         item: TransactionItemModel,
@@ -20,5 +27,14 @@ class TransactionItemViewHolder(private val view: View) :
     ) {
         this.item = item
         this.onClick = onClick
+
+        val clItemLayout = itemLayout
+        val tvSkuValue = skuValue
+        val tvAmountOriginal = amountOriginal
+        val tvAmountEuro = amountEuro
+
+        clItemLayout.setOneOffClickListener {
+            this.onClick.invoke(item, "no_action")
+        }
     }
 }
