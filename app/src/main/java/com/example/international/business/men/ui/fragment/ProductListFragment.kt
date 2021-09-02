7c2a1a6eb5f60619b94d46c7273912d73e77cf93
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-02 11:45:20 -0400

    holder (wip)

--- app/src/main/java/com/example/international/business/men/ui/fragment/ProductListFragment.kt
+++ app/src/main/java/com/example/international/business/men/ui/fragment/ProductListFragment.kt
@@ -1,12 +1,19 @@
 package com.example.international.business.men.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.international.business.men.data.model.TransactionItem
 import com.example.international.business.men.databinding.FragmentProductListBinding
+import com.example.international.business.men.ui.adapter.base.DynamicAdapter
+import com.example.international.business.men.ui.adapter.base.ItemModel
+import com.example.international.business.men.ui.adapter.item.model.ProductItemModel
+import com.example.international.business.men.ui.adapter.type.factory.TransactionItemTypeFactoryImpl
 import com.example.international.business.men.ui.viewmodel.ProductTransactionViewModel
 import com.example.international.business.men.utils.toastLong
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -22,6 +29,8 @@ class ProductListFragment : Fragment() {
 
     private var _binding: FragmentProductListBinding? = null
     private val binding get() = _binding!!
+
+    private var productAdapter: DynamicAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,19 +66,20 @@ class ProductListFragment : Fragment() {
 
         })
         productList.observe(viewLifecycleOwner, Observer {
-            if(!it.isNullOrEmpty())
+            if (!it.isNullOrEmpty()) {
                 requireActivity().toastLong("PROD - HAY DATA!")
-            else
+                setUpProductListAdapter(it)
+            } else
                 requireActivity().toastLong("PROD - NO HAY DATA!")
         })
         exchangeRateList.observe(viewLifecycleOwner, Observer {
-            if(!it.isNullOrEmpty())
+            if (!it.isNullOrEmpty())
                 requireActivity().toastLong("EX - HAY DATA!")
             else
                 requireActivity().toastLong("EX - NO HAY DATA!")
         })
         transactionList.observe(viewLifecycleOwner, Observer {
-            if(!it.isNullOrEmpty())
+            if (!it.isNullOrEmpty())
                 productTransactionViewModel.getProductList(it)
             else
                 requireActivity().toastLong("TX - NO HAY DATA!")
@@ -79,8 +89,44 @@ class ProductListFragment : Fragment() {
         })
     }
 
-    private fun setUpProductListAdapter() {
+    private fun setUpProductListAdapter(list: List<TransactionItem>) {
+        productAdapter = getProductListAdapter(list)
+        binding.rvProductList.adapter = productAdapter
+        binding.rvProductList.layoutManager = LinearLayoutManager(requireActivity())
 
+    }
+
+    private fun getProductListAdapter(list: List<TransactionItem>): DynamicAdapter {
+        return DynamicAdapter(
+            typeFactory = TransactionItemTypeFactoryImpl(),
+            items = getListForAdapter(list.toMutableList()),
+            onClick = onItemClick
+        )
+    }
+
+    private fun getListForAdapter(list: MutableList<TransactionItem>?): List<ItemModel> {
+        var result = mutableListOf<ItemModel>()
+        list?.forEach {
+            result.add(ProductItemModel(it))
+        }
+        return result
+    }
+
+    private var onItemClick: (ItemModel, String) -> Unit = { item, action ->
+        //TODO: make this leaner
+        val product: ProductItemModel = item as ProductItemModel
+        val sku = product.model.sku
+        when(action) {
+            "show_toast" -> {
+                requireActivity().toastLong("You selected the product: $sku")
+            }
+            "go_to_detail" -> {
+                requireActivity().toastLong("You're opening $sku detail")
+            }
+            else -> {
+                Log.d("TAGTAG", "message")
+            }
+        }
     }
 
 }
