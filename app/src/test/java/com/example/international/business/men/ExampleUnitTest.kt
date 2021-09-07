77ea3654a2b49714bbe6b7da08cc703fef61ea70
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-07 13:58:26 -0400

    control commit

--- app/src/test/java/com/example/international/business/men/ExampleUnitTest.kt
+++ app/src/test/java/com/example/international/business/men/ExampleUnitTest.kt
@@ -1,11 +1,13 @@
 package com.example.international.business.men
 
+import com.example.international.business.men.base.BaseUTTest
 import com.example.international.business.men.data.model.ExchangeRateItem
 import org.junit.Test
 
 import org.junit.Assert.*
 import com.example.international.business.men.data.model.TransactionItem
 import com.example.international.business.men.utils.*
+import com.google.gson.reflect.TypeToken
 
 
 /**
@@ -13,200 +15,23 @@ import com.example.international.business.men.utils.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ExampleUnitTest: BaseUTTest() {
 
     /**
      * mock data init
      */
     private fun initExchangeRateList(): List<ExchangeRateItem> {
-        var list = arrayListOf<ExchangeRateItem>()
-        list.add(ExchangeRateItem(from = "CAD", to = "AUD", rate = "0.65"))
-        list.add(ExchangeRateItem(from = "AUD", to = "CAD", rate = "1.54"))
-        list.add(ExchangeRateItem(from = "CAD", to = "EUR", rate = "0.63"))
-        list.add(ExchangeRateItem(from = "EUR", to = "CAD", rate = "1.59"))
-        list.add(ExchangeRateItem(from = "AUD", to = "USD", rate = "0.73"))
-        list.add(ExchangeRateItem(from = "USD", to = "AUD", rate = "1.37"))
+        var list: List<ExchangeRateItem> = arrayListOf<ExchangeRateItem>()
+        val dataJson = getJson("ibm_mock_rates.json")
+        list = parseArray(dataJson, object: TypeToken<List<ExchangeRateItem>>() {}.type)
         return list
     }
 
     private fun initTransactionList(): List<TransactionItem> {
-
-        var list = arrayListOf<TransactionItem>()
-
-        list.add(TransactionItem(sku = "C2149", amount = "18.7", currency = "AUD"))
-        list.add(TransactionItem(sku = "L7071", amount = "21.5", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "33.1", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "32.6", currency = "USD"))
-        list.add(TransactionItem(sku = "F1055", amount = "25.6", currency = "AUD"))
-        list.add(TransactionItem(sku = "N3391", amount = "25.9", currency = "USD"))
-        list.add(TransactionItem(sku = "W1437", amount = "28.6", currency = "CAD"))
-        list.add(TransactionItem(sku = "N6330", amount = "16.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "W1437", amount = "25.3", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "16.0", currency = "AUD"))
-        list.add(TransactionItem(sku = "W1437", amount = "27.5", currency = "EUR"))
-        list.add(TransactionItem(sku = "N5844", amount = "15.6", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "29.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "N6330", amount = "27.0", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "26.4", currency = "EUR"))
-        list.add(TransactionItem(sku = "M6639", amount = "20.9", currency = "USD"))
-        list.add(TransactionItem(sku = "I6375", amount = "27.7", currency = "USD"))
-        list.add(TransactionItem(sku = "K3549", amount = "28.0", currency = "CAD"))
-        list.add(TransactionItem(sku = "E0510", amount = "30.8", currency = "AUD"))
-        list.add(TransactionItem(sku = "I6375", amount = "15.6", currency = "AUD"))
-        list.add(TransactionItem(sku = "C2149", amount = "32.9", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "15.3", currency = "CAD"))
-        list.add(TransactionItem(sku = "B6134", amount = "20.3", currency = "EUR"))
-        list.add(TransactionItem(sku = "C4028", amount = "26.0", currency = "AUD"))
-        list.add(TransactionItem(sku = "U4760", amount = "22.6", currency = "EUR"))
-        list.add(TransactionItem(sku = "W1437", amount = "25.6", currency = "CAD"))
-        list.add(TransactionItem(sku = "I6375", amount = "31.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "C2149", amount = "27.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "U4760", amount = "20.4", currency = "AUD"))
-        list.add(TransactionItem(sku = "C2149", amount = "33.3", currency = "EUR"))
-        list.add(TransactionItem(sku = "K3549", amount = "21.7", currency = "USD"))
-        list.add(TransactionItem(sku = "U4760", amount = "30.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "W1437", amount = "22.7", currency = "USD"))
-        list.add(TransactionItem(sku = "N6330", amount = "28.3", currency = "USD"))
-        list.add(TransactionItem(sku = "F1055", amount = "15.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "N5844", amount = "21.9", currency = "USD"))
-        list.add(TransactionItem(sku = "F1055", amount = "23.3", currency = "USD"))
-        list.add(TransactionItem(sku = "N5844", amount = "31.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "C4028", amount = "19.8", currency = "AUD"))
-        list.add(TransactionItem(sku = "I6375", amount = "30.5", currency = "EUR"))
-        list.add(TransactionItem(sku = "U4760", amount = "27.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "F1055", amount = "32.2", currency = "USD"))
-        list.add(TransactionItem(sku = "C2149", amount = "26.0", currency = "CAD"))
-        list.add(TransactionItem(sku = "C4028", amount = "32.6", currency = "EUR"))
-        list.add(TransactionItem(sku = "E0510", amount = "22.8", currency = "USD"))
-        list.add(TransactionItem(sku = "N9193", amount = "16.4", currency = "USD"))
-        list.add(TransactionItem(sku = "N5844", amount = "19.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "N6330", amount = "22.4", currency = "EUR"))
-        list.add(TransactionItem(sku = "E0510", amount = "28.2", currency = "USD"))
-        list.add(TransactionItem(sku = "F1055", amount = "34.9", currency = "CAD"))
-        list.add(TransactionItem(sku = "L7071", amount = "34.3", currency = "EUR"))
-        list.add(TransactionItem(sku = "L7071", amount = "16.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "E0510", amount = "29.3", currency = "CAD"))
-        list.add(TransactionItem(sku = "I6375", amount = "26.7", currency = "EUR"))
-        list.add(TransactionItem(sku = "M6639", amount = "16.2", currency = "USD"))
-        list.add(TransactionItem(sku = "C2149", amount = "32.3", currency = "USD"))
-        list.add(TransactionItem(sku = "M6639", amount = "17.5", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "21.0", currency = "EUR"))
-        list.add(TransactionItem(sku = "C2149", amount = "17.7", currency = "CAD"))
-        list.add(TransactionItem(sku = "N9193", amount = "22.2", currency = "USD"))
-        list.add(TransactionItem(sku = "B6134", amount = "27.7", currency = "AUD"))
-        list.add(TransactionItem(sku = "L7071", amount = "34.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "33.3", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "19.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "E0510", amount = "30.9", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "28.7", currency = "EUR"))
-        list.add(TransactionItem(sku = "I6375", amount = "31.1", currency = "CAD"))
-        list.add(TransactionItem(sku = "E0510", amount = "21.4", currency = "CAD"))
-        list.add(TransactionItem(sku = "N5844", amount = "34.1", currency = "CAD"))
-        list.add(TransactionItem(sku = "C4028", amount = "34.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "F1055", amount = "33.3", currency = "USD"))
-        list.add(TransactionItem(sku = "N9193", amount = "17.1", currency = "AUD"))
-        list.add(TransactionItem(sku = "M6639", amount = "28.4", currency = "AUD"))
-        list.add(TransactionItem(sku = "C2149", amount = "34.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "N5844", amount = "28.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "W1437", amount = "32.1", currency = "AUD"))
-        list.add(TransactionItem(sku = "L7071", amount = "17.2", currency = "CAD"))
-        list.add(TransactionItem(sku = "B6134", amount = "15.1", currency = "CAD"))
-        list.add(TransactionItem(sku = "C2149", amount = "21.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "N9193", amount = "27.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "I6375", amount = "15.2", currency = "CAD"))
-        list.add(TransactionItem(sku = "B6134", amount = "26.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "K3549", amount = "20.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "16.2", currency = "CAD"))
-        list.add(TransactionItem(sku = "L7071", amount = "28.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "L7071", amount = "25.8", currency = "EUR"))
-        list.add(TransactionItem(sku = "C2149", amount = "27.7", currency = "EUR"))
-        list.add(TransactionItem(sku = "K3549", amount = "16.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "U4760", amount = "19.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "28.8", currency = "AUD"))
-        list.add(TransactionItem(sku = "N9193", amount = "30.9", currency = "CAD"))
-        list.add(TransactionItem(sku = "L7071", amount = "22.3", currency = "EUR"))
-        list.add(TransactionItem(sku = "E0510", amount = "22.9", currency = "AUD"))
-        list.add(TransactionItem(sku = "N3391", amount = "16.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "N9193", amount = "32.3", currency = "USD"))
-        list.add(TransactionItem(sku = "M6639", amount = "31.4", currency = "EUR"))
-        list.add(TransactionItem(sku = "L7071", amount = "19.0", currency = "CAD"))
-        list.add(TransactionItem(sku = "N5844", amount = "22.6", currency = "AUD"))
-        list.add(TransactionItem(sku = "C4028", amount = "34.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "N3391", amount = "15.4", currency = "AUD"))
-        list.add(TransactionItem(sku = "M6639", amount = "24.0", currency = "CAD"))
-        list.add(TransactionItem(sku = "E0510", amount = "33.4", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "32.0", currency = "CAD"))
-        list.add(TransactionItem(sku = "U4760", amount = "20.7", currency = "AUD"))
-        list.add(TransactionItem(sku = "C2149", amount = "29.7", currency = "CAD"))
-        list.add(TransactionItem(sku = "N9193", amount = "20.3", currency = "EUR"))
-        list.add(TransactionItem(sku = "K3549", amount = "32.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "V5497", amount = "22.7", currency = "CAD"))
-        list.add(TransactionItem(sku = "U4760", amount = "23.5", currency = "USD"))
-        list.add(TransactionItem(sku = "B6134", amount = "28.9", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "20.8", currency = "EUR"))
-        list.add(TransactionItem(sku = "K3549", amount = "34.2", currency = "CAD"))
-        list.add(TransactionItem(sku = "C4028", amount = "26.8", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "22.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "N5844", amount = "34.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "N5844", amount = "16.9", currency = "CAD"))
-        list.add(TransactionItem(sku = "N5844", amount = "31.6", currency = "USD"))
-        list.add(TransactionItem(sku = "B6134", amount = "25.2", currency = "CAD"))
-        list.add(TransactionItem(sku = "N3391", amount = "31.6", currency = "USD"))
-        list.add(TransactionItem(sku = "I6375", amount = "19.7", currency = "EUR"))
-        list.add(TransactionItem(sku = "N3391", amount = "20.4", currency = "CAD"))
-        list.add(TransactionItem(sku = "U4760", amount = "17.7", currency = "EUR"))
-        list.add(TransactionItem(sku = "I6375", amount = "24.0", currency = "USD"))
-        list.add(TransactionItem(sku = "C2149", amount = "22.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "B6134", amount = "15.5", currency = "USD"))
-        list.add(TransactionItem(sku = "K3549", amount = "32.1", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "17.0", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "17.5", currency = "CAD"))
-        list.add(TransactionItem(sku = "N6330", amount = "18.5", currency = "CAD"))
-        list.add(TransactionItem(sku = "N6330", amount = "17.3", currency = "CAD"))
-        list.add(TransactionItem(sku = "N9193", amount = "19.7", currency = "USD"))
-        list.add(TransactionItem(sku = "B6134", amount = "23.5", currency = "USD"))
-        list.add(TransactionItem(sku = "C4028", amount = "32.9", currency = "USD"))
-        list.add(TransactionItem(sku = "C4028", amount = "19.6", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "20.4", currency = "USD"))
-        list.add(TransactionItem(sku = "K3549", amount = "25.6", currency = "AUD"))
-        list.add(TransactionItem(sku = "F1055", amount = "26.0", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "17.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "C2149", amount = "20.2", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "17.7", currency = "AUD"))
-        list.add(TransactionItem(sku = "B6134", amount = "22.8", currency = "AUD"))
-        list.add(TransactionItem(sku = "B6134", amount = "25.9", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "25.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "C4028", amount = "16.9", currency = "CAD"))
-        list.add(TransactionItem(sku = "F1055", amount = "30.3", currency = "USD"))
-        list.add(TransactionItem(sku = "I6375", amount = "19.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "W1437", amount = "28.8", currency = "USD"))
-        list.add(TransactionItem(sku = "W1437", amount = "27.2", currency = "EUR"))
-        list.add(TransactionItem(sku = "I6375", amount = "15.3", currency = "USD"))
-        list.add(TransactionItem(sku = "U4760", amount = "23.4", currency = "CAD"))
-        list.add(TransactionItem(sku = "L7071", amount = "24.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "L7071", amount = "19.6", currency = "USD"))
-        list.add(TransactionItem(sku = "K3549", amount = "28.5", currency = "CAD"))
-        list.add(TransactionItem(sku = "W1437", amount = "33.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "M6639", amount = "16.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "B6134", amount = "31.1", currency = "CAD"))
-        list.add(TransactionItem(sku = "V5497", amount = "30.4", currency = "AUD"))
-        list.add(TransactionItem(sku = "N3391", amount = "27.7", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "31.1", currency = "EUR"))
-        list.add(TransactionItem(sku = "N5844", amount = "15.3", currency = "AUD"))
-        list.add(TransactionItem(sku = "C4028", amount = "16.8", currency = "EUR"))
-        list.add(TransactionItem(sku = "N6330", amount = "28.0", currency = "AUD"))
-        list.add(TransactionItem(sku = "K3549", amount = "29.5", currency = "USD"))
-        list.add(TransactionItem(sku = "C2149", amount = "27.1", currency = "USD"))
-        list.add(TransactionItem(sku = "N9193", amount = "32.0", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "33.6", currency = "CAD"))
-        list.add(TransactionItem(sku = "W1437", amount = "31.4", currency = "EUR"))
-        list.add(TransactionItem(sku = "B6134", amount = "25.1", currency = "USD"))
-        list.add(TransactionItem(sku = "C4028", amount = "22.6", currency = "USD"))
-        list.add(TransactionItem(sku = "N3391", amount = "20.5", currency = "AUD"))
-        list.add(TransactionItem(sku = "I6375", amount = "22.8", currency = "CAD"))
-        list.add(TransactionItem(sku = "N6330", amount = "28.3", currency = "USD"))
-        list.add(TransactionItem(sku = "E0510", amount = "15.3", currency = "USD"))
-
+        var list: List<TransactionItem> = arrayListOf<TransactionItem>()
+        val dataJson = getJson("ibm_mock_transactions.json")
+        val type = object: TypeToken<List<TransactionItem>>() {}.type
+        list = parseArray(dataJson, type)
         return list
     }
 
@@ -252,7 +77,7 @@ class ExampleUnitTest {
     @Test
     fun evaluate_missingRatesAux() {
 
-        val mockSku = "N6330"
+        val mockSku = "R3990"
         val mockToCurrency = CURRENCY_EUR
 
         //obtain mock rate list
@@ -282,7 +107,7 @@ class ExampleUnitTest {
     @Test
     fun evaluate_missingRatesFun() {
 
-        val mockSku = "N6330"
+        val mockSku = "R3990"
         val mockToCurrency = CURRENCY_EUR
 
         //obtain mock rate list
