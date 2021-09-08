4aec08641333fba7bc89ad741fda467d8dc05966
Christopher Serrano <christopher.serrano@alphas.technology>
2021-09-08 15:34:40 -0400

    Main coroutine rule (experimental)

--- app/src/test/java/com/example/international/business/men/utils/ExchangeRateItemUtilsTest.kt
+++ app/src/test/java/com/example/international/business/men/utils/ExchangeRateItemUtilsTest.kt
@@ -1,16 +1,41 @@
 package com.example.international.business.men.utils
 
+import com.example.international.business.men.base.BaseUTTest
+import com.example.international.business.men.data.model.ExchangeRateItem
+import com.example.international.business.men.di.configureAppTestModules
+import com.google.gson.reflect.TypeToken
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
 
-class ExchangeRateItemUtilsTest {
+class ExchangeRateItemUtilsTest: BaseUTTest(), KoinTest {
+
+    private val exchangeRateUtils: ExchangeRateItemUtils by inject()
+    private val currency = CURRENCY_EUR
+
+    @Before
+    fun start() {
+        super.setUp()
+        startKoin { modules(configureAppTestModules(getMockWebServerUrl())) }
+    }
 
     @Test
     fun obtainMissingExchangePairs() {
+        val ratesJson = getJson("ibm_mock_rates.json")
+        val rates: List<ExchangeRateItem> =
+            parseArray(ratesJson, object : TypeToken<List<ExchangeRateItem>>() {}.type)
+        assertNotNull(rates)
     }
 
     @Test
     fun calculateMissingExchangeRates() {
+        val ratesJson = getJson("ibm_mock_rates.json")
+        val rates: List<ExchangeRateItem> =
+            parseArray(ratesJson, object : TypeToken<List<ExchangeRateItem>>() {}.type)
+        assertNotNull(rates)
     }
 }
